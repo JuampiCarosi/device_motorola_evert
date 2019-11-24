2b261e29ba82f51a7d1d67d1f1cc40908fdee1a3
trpsl <trpsl755@gmail.com>
2019-11-24 18:19:52 +0100

    evert: enable eis, disable ois

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -32,6 +32,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.fastbootd.available=true
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.eis.enable=1 \
+    persist.vendor.camera.ois.disable=1
+
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.fm.use_audio_session=true
